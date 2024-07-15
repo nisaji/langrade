fab2d56962e1b8655fbebfa618a0e0b3cc474bad
nisaji <nisaji27@gmail.com>
2024-07-15 18:13:55 +0900

    fix: import

--- langrade/__init__.py
+++ langrade/__init__.py
@@ -1,4 +1,4 @@
-from .grader import document_grader
+from .grader import document_grader, DocumentGrader
 from .retriever import create_retriever
 
-__all__ = ["document_grader", "create_retriever"]
+__all__ = ["document_grader", "DocumentGrader", "create_retriever"]
