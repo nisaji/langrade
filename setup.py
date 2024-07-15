34a4fc72b0f0ec364252cadc6abdc01e293360e7
nisaji <nisaji27@gmail.com>
2024-07-15 20:24:21 +0900

    fix: version 0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="langrade",
-    version="0.1.1",
+    version="0.1.2",
     author="Your Name",
     author_email="your.email@example.com",
     description="A library for grading and retrieving documents based on relevance",  # noqa: E501
